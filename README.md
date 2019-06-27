74ab1a83a53410f17c71502c5e6046d2f14d0afe
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-27 15:55:49 -0700

    created server, able to read http packet sent back!

--- README.md
+++ README.md
@@ -24,7 +24,14 @@ NET to HTTP server
 1. Create html pages to serve up
     1. COMMIT
 
+1. Make a JSON dog file????
+    1. COMMIT
+
+1. JSON THINGS?
+1. COMMIT
+
 1. Create tests! 
     1. COMMIT
 
+1. Import necessary lib/mods: fs, net, ??
     1. COMMIT
--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-// const app = require('app');
-
-// app.listen(8080, () => {
-//     res.end('you did it!');
-// })
-
-//new
 const { createServer } = require('net');
 
 const server = createServer(sock => {
-    console.log('client connected');
-    sock.write('I will echo');
-    // sock.end()
+    console.log('Server has uhhhh been started? I think?');
+    // sock.write('I will echo');
+    // // sock.end()
 
     sock.on('data', data => {
-        // console.log('from client side', data.toString())
-        sock.write(data.toString());
+        console.log('from client side', data.toString());
+        // sock.write(data.toString());
     });
 });
 
