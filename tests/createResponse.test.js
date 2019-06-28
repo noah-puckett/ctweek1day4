676484f2ccf31e38d554c35a6878c19f31f6baa4
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-28 13:40:32 -0700

    passed createResponse test so far

--- tests/createResponse.test.js
+++ tests/createResponse.test.js
@@ -3,34 +3,50 @@ const { createResponse } = require('../lib/utils/makeHTTPPacket');
 describe('create response', () => {
 
     it('if given a request, returns correctly interpolated response', () => {
+        global.Date = jest.fn(() => {
+            return '1234';
+        });
         
         const contentType = {
             json: 'application/json',
             html: 'text/html'
         };
 
-        const request = `GET / HTTP/1.1
-        Date: ${new Date()}
-        Accept-Ranges: bytes
-        Content-Length: 290
-        Connection: Keep-Alive
-        Content-Type: text/html`;
-        
-        const expected = `<!DOCTYPE html>
+        const request = `<!DOCTYPE html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Document</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
         </head>
         <body>
-            <h1>YOU HAVE SUCCEEDED</h1>
+        <h1>YOU HAVE SUCCEEDED</h1>
+        </body>
+        </html>`;
+        
+        const expected = `HTTP/1.1 200 OK
+Date: ${new Date()}
+Accept-Ranges: bytes
+Content-Length: 358
+Connection: Keep-Alive
+Content-Type: text/html
+
+<!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        </head>
+        <body>
+        <h1>YOU HAVE SUCCEEDED</h1>
         </body>
         </html>`;
 
         const result = createResponse('200 OK', request, contentType.html);
 
-        expected(expected).toEqual(result);
+        expect(expected).toEqual(result);
     }); 
 });
---
+++ tests/parseData.test.js
@@ -0,0 +1,13 @@
+describe('parse data function',  () => {
+
+    it('takes unparsed data and returns correctly formatted data', () => {
+        const data = '';
+
+        const expected = '';
+
+        const result = '';
+
+        expect(expected).toEqual(result);
+    });
+
+});
