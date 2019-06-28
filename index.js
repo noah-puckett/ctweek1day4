14dcb95de4151e99c60587e5afcd3a5ef50420f3
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-28 15:16:30 -0700

    added more public pages, added logic for POST and bad path requests, made a json dog

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const { createServer } = require('net');
 const { createResponse } = require('./lib/utils/makeHTTPPacket');
 const { parseData } = require('./lib/utils/parseData');
+const { sitesArray } = require('./public/public');
 
 const contentType = {
     json: 'application/json',
     html: 'text/html'
 };
 
-const index = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>YOU HAVE SUCCEEDED</h1>
-</body>
-</html>`;
-
 const server = createServer(sock => {
-    console.log('Server has uhhhh been started? I think?');
+    console.log('Server has been started');
 
     sock.on('data', data => {
+        
         const parsedData = parseData(data); 
-        console.log(parsedData);
+
         if(parsedData.path === '/') {
-            const response = createResponse('200 OK', index, contentType.html);
+            const response = createResponse('200 OK', sitesArray.index, contentType.html);
+            sock.write(response);
+            sock.end();
+        }
+        else if(parsedData.path === '/red') {
+            const response = createResponse('200 OK', sitesArray.red, contentType.html);
+            sock.write(response);
+            sock.end();
+        }
+        else if(parsedData.path === '/blue') {
+            const response = createResponse('200 OK', sitesArray.blue, contentType.html);
+            sock.write(response);
+            sock.end();
+        }
+        else if(parsedData.path === '/dog') {
+            const response = createResponse('200 OK', JSON.stringify(sitesArray.dog), contentType.json);
+            sock.write(response);
+            sock.end();
+        }
+        else if(parseData.method === 'POST') {
+            const response = createResponse('400', '<h1>Sorry! Absofruitly not!</h1>', contentType.html);
             sock.write(response);
             sock.end();
         }
         else {
+            const response = createResponse('404', sitesArray.notFound, contentType.html);
+            sock.write(response);
+            sock.end();
             console.log('SOMETHING IS ROTTEN IN DENMARK');
         }
     });
